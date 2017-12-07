5c5c6828666bab582850e59b65fe37a3c9251b9d
Sadman Sakib Saumik <>
2017-12-07 12:50:12 +0600

    GoalVC implemented and outlets setup

--- GoalPost/Controller/GoalVC.swift
+++ GoalPost/Controller/GoalVC.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addGoalsButtonPressed(_ sender: Any) {
     }
-
-
+    
+    
 }
 
